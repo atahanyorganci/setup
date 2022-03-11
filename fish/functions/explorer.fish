2458d3f74ec0377643870b2b4ada054f8b00e65a
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-03-11 16:56:03 +0300

    feat(fish): if no arguments are provided `explorer` function opens current directory

--- fish/functions/explorer.fish
+++ fish/functions/explorer.fish
@@ -1,3 +1,8 @@
 function explorer --description "Windows Explorer"
-    $WIN_ROOT/Windows/explorer.exe $argv
+    set -l cmd "$WIN_ROOT/Windows/explorer.exe"
+    if test (count $argv) == 0
+        command $exe .
+    else
+        command $exe $argv
+    end
 end
