16b8819a42bc4309ca752723cf36fd31378dcefb
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-03-25 16:14:12 +0300

    feat(fish): add `fish_title` function
    `fish_title` function can be used set the title of the terminal. Current
    implementation tries to display current directory relative to `$HOME`
    and defaults to `$PWD`. If a process is running it will be displayed
    in the title '[$process] @ $path'.

--- fish/config.fish
+++ fish/config.fish
@@ -43,6 +43,19 @@ function ll
     command exa -l --git-ignore $argv
 end
 
+function fish_title
+    if set -q argv[1]
+        set -l arg $argv[1]
+        echo "[$arg] @ "
+    end
+    set -l escaped (string replace -a "/" "\/" $HOME)
+    if string match -rq "$escaped(?<path>.*)" $PWD
+        echo "~$path"
+    else
+        echo $PWD
+    end
+end
+
 set -U OS (os)
 
 # If configuring fish for Mac OS add brew to PATH
