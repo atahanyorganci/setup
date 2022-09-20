847687c0d04e1a6ad41268247ecff068f3512790
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-09-20 15:01:16 +0200

    refactor(ipython): add new `.gitignore` and format `ipython_config.py`

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,7 +1,9 @@
+# type: ignore
+
 c.TerminalIPythonApp.display_banner = False
 c.InteractiveShellApp.exec_lines = [
-    'from pathlib import Path',
-    'import math',
-    'import os',
+    "from pathlib import Path",
+    "import math",
+    "import os",
 ]
 c.TerminalInteractiveShell.confirm_exit = False
