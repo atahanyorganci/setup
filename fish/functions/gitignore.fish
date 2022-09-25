9c19313529b820d6275cdf09672cd36971bcbf80
Atahan Yorgancı <atahanyorganci@hotmail.com>
2022-09-25 20:19:40 +0200

    refactor(fish): reimplement `quick_ignore` with saner defaults

--- fish/functions/gitignore.fish
+++ fish/functions/gitignore.fish
@@ -1,11 +1,11 @@
-function quick_ignore --description 'Choose and copy .gitignore from github/gitignore'
-    argparse -n quick_ignore i/init u/update l/list -- $argv
+function gitignore --description 'Choose and copy .gitignore from github/gitignore'
+    argparse -n gitignore i/init u/update l/list d/dest= -- $argv
 
     # github/gitignore should be locally available
     set -l data_dir $HOME/.local/share
-    set -l repo $data_dir/gitignore/
+    set -l repo $data_dir/gitignore
 
-    # Initialize `quick_ignore` by cloning github/gitignore to $HOME/.local/share/gitignore
+    # Initialize `gitignore` by cloning github/gitignore to $HOME/.local/share/gitignore
     if test -n "$_flag_init"
         printf "Downloading github/gitignore to %s..." $repo
         mkdir -p $repo && git clone "https://github.com/github/gitignore" $repo >/dev/null 2>&1
@@ -14,7 +14,7 @@ function quick_ignore --description 'Choose and copy .gitignore from github/giti
             return
         else
             printf " FAILED!\n"
-            echo "run quick_ignore --init to try again"
+            echo "run gitignore --init to try again"
             return 1
         end
     end
@@ -22,11 +22,11 @@ function quick_ignore --description 'Choose and copy .gitignore from github/giti
     # If $HOME/.local/share/gitignore isn't a directory, exit with error
     if test ! -d $repo
         echo "github/gitignore not found in $repo"
-        echo "run quick_ignore --init to download it"
+        echo "run gitignore --init to download it"
         return 1
     end
 
-    # Update `quick_ignore` by pulling latest changes from github/gitignore
+    # Update `gitignore` by pulling latest changes from github/gitignore
     if test -n "$_flag_update"
         printf "Updating github/gitignore in %s..." $repo
         set -l last $PWD
@@ -38,23 +38,29 @@ function quick_ignore --description 'Choose and copy .gitignore from github/giti
             return
         else
             printf " FAILED!\n"
-            echo "run quick_ignore --update to try again"
+            echo "run gitignore --update to try again"
             return 1
         end
     end
 
-    set -l list (ll $HOME/.local/share/gitignore/*.gitignore | awk '{ n = split($7, a, "/"); print a[n]; }')
+    if test -n "$_flag_dest"
+        set -f dest "$_flag_dest"
+    else
+        set -f dest ".gitignore"
+    end
+
+    set -l list (ll $repo/*.gitignore | awk '{ n = split($7, a, "/"); print a[n]; }')
     if test -n "$_flag_list"
         echo "Available gitignore files:"
         printf "%s\n" $list
     else
-        if test (count $argv) != 1
-            echo "Usage: quick_ignore [destination]"
-            return 1
+        if test (count $argv) = 1
+            set -f file $argv[1]
+        else
+            set -f file ""
         end
-        set -l dest $argv[1]
-        set -l ignore (printf "%s\n" $list | fzf)
-        echo "Copying '$ignore'"
+        set -l ignore (printf "%s\n" $list | fzf --query="$file" --preview="bat $repo/{} -l gitignore ")
+        echo "Copying '$ignore' to $dest"
         if test -z "$ignore"
             return 1
         else
