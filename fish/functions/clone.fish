a50d1a28115d7b066865ffa50a9205fa711688e3
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-02-13 17:12:25 +0100

    feat(fish): add ability to clone from GitHub URLs to `clone` function

--- fish/functions/clone.fish
+++ fish/functions/clone.fish
@@ -1,17 +1,28 @@
+function __clone_github
+    set -f user $argv[1]
+    set -f repo $argv[2]
+    set -f repo_dir "$DEV_HOME/github/$user/$repo"
+    info "Cloning $user/$repo into $repo_dir"
+
+    mkdir -p $repo_dir
+    gh repo clone $user/$repo $repo_dir || return $status
+end
+
 function clone --description 'Clone git repository into development dir'
     argparse -n=clone -N 1 -X 1 g/github git -- $argv || return $status
 
     debug "Using $DEV_HOME as development directory"
 
-    if test -n "$_flag_github"
-        set -l parts (string split '/' $argv[1])
-        set -l user $parts[1]
-        set -l repo $parts[2]
-        set -l repo_dir "$DEV_HOME/github/$user/$repo"
-        info "Cloning $user/$repo into $repo_dir"
-
-        mkdir -p $repo_dir
-        gh repo clone $user/$repo $repo_dir || return $status
+    if string match -q -r 'https?:\/\/github.com\/(?<user>\w+)\/(?<repo>\w+)(\.git)?' $argv[1]
+        debug "GitHub repository detected as $user/$repo"
+        __clone_github $user $repo || return $status
+    else if test -n "$_flag_github"
+        if string match -q -r '(?<user>\w+)\/(?<repo>\w+)' $argv[1]
+            debug "User: $user, Repo: $repo"
+            __clone_github $user $repo || return $status
+        else
+            fatal "Invalid GitHub repository!" && return 1
+        end
     else if test -n "$_flag_git"
         set -l parts (string split '/' $argv[1])
         set -l name (string replace '.git' '' $parts[-1])
