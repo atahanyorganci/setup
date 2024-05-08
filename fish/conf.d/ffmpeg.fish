5825086e31fdf1ea07563a28419b346cc7fc375b
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-05-08 20:15:51 +0200

    docs(fish): add description to `ffmpeg` and `ffprobe` aliases

--- fish/conf.d/ffmpeg.fish
+++ fish/conf.d/ffmpeg.fish
@@ -1,7 +1,7 @@
-function ffmpeg --wraps ffmpeg
+function ffmpeg --wraps ffmpeg --description "FFmpeg with no banner"
     command ffmpeg -hide_banner $argv
 end
 
-function ffprobe --wraps ffprobe
+function ffprobe --wraps ffprobe --description "FFprobe with no banner"
     command ffprobe -hide_banner $argv
 end
