f49a2b6e0f0711685959506b0a8df049904714ce
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-08-27 01:20:38 +0300

    feat(nix-darwin): add `home-manager` to lock file

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -4,8 +4,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nix-darwin, nixpkgs, }: {
+  outputs = inputs@{ self, nix-darwin, nixpkgs, ... }: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Atahan-MacBook-Pro
     darwinConfigurations."Atahan-MacBook-Pro" = nix-darwin.lib.darwinSystem {
