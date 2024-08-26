a0ead04de83adf949ddebeeeeeacd6a16c87da40
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-08-26 15:54:58 +0300

    chore(fish): remove `mise` config since it is replaced by `nix`

--- fish/config.fish
+++ fish/config.fish
@@ -80,11 +80,6 @@ set -Ux PANDOC_DATA_DIR (pandoc -v | grep data | awk -F: '{ gsub(/ /,""); print 
 set -Ux FLY_INSTALL "$XDG_DATA_HOME/fly"
 fish_add_path "$FLY_INSTALL/bin" $PATH
 
-# `mise` development tool manager
-if not set -q IN_NIX_SHELL
-    mise activate fish | source
-end
-
 set -Ux DOCKER_CONFIG_HOME "$XDG_CONFIG_HOME/docker"
 set -Ux WGETRC "$XDG_CONFIG_HOME/wgetrc"
 alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
