47d49b557c996a49310182f3ba51255d0d8df647
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-08-28 13:34:31 +0300

    feat(nix): configure Fish plugins via home-manager

--- nix/modules/shell.nix
+++ nix/modules/shell.nix
@@ -2,5 +2,36 @@
 {
   programs.bash.enable = true;
   programs.zsh.enable = true;
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "autopair.fish";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "autopair.fish";
+          rev = "1.0.4";
+          sha256 = "sha256-s1o188TlwpUQEN3X5MxUlD/2CFCpEkWu83U9O+wg3VU=";
+        };
+      }
+      {
+        name = "gitnow";
+        src = pkgs.fetchFromGitHub {
+          owner = "joseluisq";
+          repo = "gitnow";
+          rev = "2.12.0";
+          sha256 = "sha256-PuorwmaZAeG6aNWX4sUTBIE+NMdn1iWeea3rJ2RhqRQ=";
+        };
+      }
+      {
+        name = "sponge";
+        src = pkgs.fetchFromGitHub {
+          owner = "meaningful-ooo";
+          repo = "sponge";
+          rev = "1.1.0";
+          sha256 = "sha256-MdcZUDRtNJdiyo2l9o5ma7nAX84xEJbGFhAVhK+Zm1w=";
+        };
+      }
+    ];
+  };
 }
