d7fb9f465dffc24e1805575d69acb639d594b628
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-08-29 16:01:06 +0300

    feat(fish): make `open` function cross-platform

--- fish/functions/open.fish
+++ fish/functions/open.fish
@@ -1,7 +1,13 @@
 function open --description 'Open file in its default application'
+    switch (os)
+        case Darwin
+            set -f command open
+        case "*Linux*"
+            set -f command xdg-open
+    end
     if test (count $argv) -eq 0
-        command xdg-open .
+        command $command .
     else
-        command xdg-open $argv
+        command $command $argv
     end
 end
