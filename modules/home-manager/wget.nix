7f22315907f9fe017aaf74188893a69e913b9a2b
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-08-30 17:18:05 +0300

    fix: add missin `lib.mkIf` for `wget`

--- modules/home-manager/wget.nix
+++ modules/home-manager/wget.nix
@@ -4,8 +4,8 @@
 , ...
 }:
 {
-  options.wget = lib.mkEnableOption "wget";
-  config = {
+  options.wget.enable = lib.mkEnableOption "wget";
+  config = lib.mkIf config.wget.enable {
     home.file."${config.xdg.configHome}/wgetrc".text = "";
     home.shellAliases.wget = "${pkgs.wget}/bin/wget --hsts-file=${config.xdg.cacheHome}/wget-hsts";
     home.sessionVariables.WGETRC = "${config.xdg.configHome}/wgetrc";
