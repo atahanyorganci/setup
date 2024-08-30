c1864fbc0909b4c69cfbecea448faa5d28441c62
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-08-30 15:43:59 +0300

    feat: configure MacOS dock to autohide and not show recent applications

--- hosts/macbook-pro/default.nix
+++ hosts/macbook-pro/default.nix
@@ -26,6 +26,13 @@ in
   system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
   # Ensures compatibility with defaults from NixOS
   system.stateVersion = 4;
+  # Dock configuration
+  system.defaults = {
+    dock = {
+      autohide = true;
+      show-recents = false;
+    };
+  };
   # Users managed by Nix
   users.knownUsers = [ user.username ];
   users.users.${user.username} = {
