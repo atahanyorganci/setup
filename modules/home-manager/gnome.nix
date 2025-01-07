a792b40bae6ec86b2a07fb6c5aece86e599b78e9
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-01-07 22:11:44 +0300

    fix: formatting issue found by `nixpkgs-fmt`

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -1,7 +1,7 @@
 { lib, config, ... }:
 {
   options.gnome.enable = lib.mkEnableOption "Gnome desktop environment";
-  config = lib.mkIf config.gnome.enable {   
+  config = lib.mkIf config.gnome.enable {
     dconf = {
       enable = true;
       settings."org/gnome/desktop/interface".color-scheme = lib.mkForce "prefer-dark";
