1c1a2d7620eb0976bf60ebb406252149071cc202
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-05-15 23:22:15 +0300

    fix: update `vscode` module to work with `nix-casks` and `home-manager`

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -5,18 +5,22 @@
 , ...
 }:
 let
+  isDarwin = lib.strings.hasSuffix "darwin" pkgs.system;
+  isLinux = lib.strings.hasSuffix "linux" pkgs.system;
+  caskName = "visual-studio-code";
   pkg =
-    if lib.strings.hasSuffix "darwin" pkgs.system
-    then inputs.nix-casks.packages.${pkgs.system}.visual-studio-code
+    if isDarwin
+    then inputs.nix-casks.packages.${pkgs.system}.${caskName}
     else pkgs.vscode;
 in
 {
   options.vscode.enable = lib.mkEnableOption "Visual Studio Code";
   config = lib.mkIf config.vscode.enable {
-    home.sessionVariables = {
-      EDITOR = "code --wait";
+    home = {
+      packages = [ pkg ];
+      sessionVariables.EDITOR = "code --wait";
     };
-    programs.vscode = {
+    programs.vscode = lib.mkIf isLinux {
       package = pkg;
       enable = true;
       enableUpdateCheck = true;
