755f4513e6895db1b99206818ea80edfb50448df
Atahan Yorgancı <atahan.yorganci@synnada.ai>
2025-07-20 18:31:33 +0300

    feat: add `flake-parts` as input

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "NixOS configuration for Atahan's MacBook Pro";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
