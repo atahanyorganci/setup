b892668d2e8acb81f9ffd47d05c6893d907951d5
Atahan Yorgancı <atahanyorganci@hotmail.com>
2025-07-31 22:37:36 +0300

    feat: enable GPG agent service

--- modules/home-manager/gpg/default.nix
+++ modules/home-manager/gpg/default.nix
@@ -8,6 +8,10 @@
     enable = true;
     includes = [ "~/.orbstack/ssh/config" ];
   };
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+  };
   home.sessionVariables = {
     SSH_AUTH_SOCK = "${config.home.homeDirectory}/.gnupg/S.gpg-agent.ssh";
   };
